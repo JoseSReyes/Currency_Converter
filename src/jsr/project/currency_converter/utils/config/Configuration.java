c32420d46f31b23fdac60186f41f78810c067a76
JoseSReyes <92191906+JoseSReyes@users.noreply.github.com>
2024-10-15 16:10:08 -0600

    Update Configuration.java

--- src/jsr/project/currency_converter/utils/config/Configuration.java
+++ src/jsr/project/currency_converter/utils/config/Configuration.java
@@ -1,5 +1,5 @@
 package jsr.project.currency_converter.utils.config;
 
 public class Configuration {
-    public static final String API_KEY = "e98c0da509dffd6e2e9384b7";
+    public static final String API_KEY = "TU_API_KEY";
 }
